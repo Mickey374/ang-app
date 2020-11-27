314a5e0bd83cadff5a37605b236e40e05e168538
Mickey374 <asantemichael37483@gmail.com>
2020-11-27 09:34:27 +0000

    Created Interface as a TypeScript and imported in the component.ts, also created two functions: AddItem() and deleteItem() to handle the To-Do list commands

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Todo } from "./todo";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dev-Mickey Project';
-  date = new Date();
+  
+  todoValue:string;
+  list: Todo[];
+
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+
+  }
+
+  addItem(){
+    if(this.todoValue !== ""){
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number){
+    this.list = this.list.filter(item => item.id !==id);
+    
+  }
 }
